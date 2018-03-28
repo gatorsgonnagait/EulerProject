d9c3248b91e8d1a709eb81b2d21f305db8ba1810
evan <evmesa@gmail.com>
2018-03-28 12:27:18 -0700

    number 9

---
+++ 9_Find_Pythagorean_Triple.py
@@ -0,0 +1,19 @@
+
+
+def find_triple_given_sum(sum):
+    a = 1
+    while a <= sum/3:
+
+        b = a + 1
+        while b <= sum/2:
+
+            c = sum - a - b
+            print(c)
+            if ( a*a + b*b == c*c):
+                return a * b * c
+            b+=1
+
+        a+=1
+
+
+print(find_triple_given_sum(1000))
