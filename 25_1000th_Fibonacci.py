85a7a14cc4075c65f7c7d00c0e6cf3813ef9d361
evmes <evmesa@gmail.com>
2018-04-19 10:42:28 -0700

    17

---
+++ 25_1000th_Fibonacci.py
@@ -0,0 +1,23 @@
+
+def fibonacci(n):
+
+    array = [0,1]
+    for i in range(2, n+1):
+
+        array.append( array[i-1] + array[i-2] )
+
+    return  array[n]
+
+
+def find_nth_digit_length(n):
+
+    i = 0
+    while True:
+
+        fib_answer = fibonacci(i)
+        if len(str(fib_answer)) == n:
+            return i
+        i+=1
+
+
+print(find_nth_digit_length(1000))
